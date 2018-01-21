46c5ee5798e879c910959911ee9da24802ba0d0f
git-rishabh <rishabh999gupta@gmail.com>
2018-01-21 19:58:53 +0530

    Update nlp_score.py

--- nlp_score.py
+++ nlp_score.py
@@ -2,7 +2,7 @@ import pickle
 from Question import Question
 from loc_score import *
 
-with open("answer_texts_by_question_id4.dump","rb") as f1:
+with open("mid_so_answer_texts_by_question_id4.dump","rb") as f1:
 	answers = pickle.load(f1)
 
 # print len(answers)
@@ -10,31 +10,27 @@ with open("answer_texts_by_question_id4.dump","rb") as f1:
 # 	print len(answers[i])
 # print len(answers[0])
 nlp_score_list=dict()
-with open("questions4.dump","rb") as f2:
+with open("mid_so_questions4.dump","rb") as f2:
 	questions = pickle.load(f2)
 print "Dump Read ..."
-f=open("nlp_score.txt","wb")
-c=1
+f=open("mid_so_nlp_score.txt","wb")
+
 for ques_id in questions.keys():
-	print c,ques_id,
+	print ques_id,
+	tot_score=0
 	try:
 		ans_list=answers[ques_id]
-		tot_score=0
-		for ans in ans_list:
-			try:
-				score = nlp_score(ans)
-				if(score!=-1):
-					tot_score = tot_score+score
-			except:
-				pass
-		final_score=tot_score/len(ans_list)
-		nlp_score_list[ques_id]=final_score
-		print final_score
-		with open("nlp_score_list.dump","wb") as f3:
-			pickle.dump(nlp_score_list,f3)
-		f.write(ques_id+" "+str(final_score)+"\n")
+		score = nlp_score(ans_list[0])
+		if(score!=-1):
+			tot_score = score
 	except:
 		pass
-	c=c+1
+	final_score=tot_score
+	nlp_score_list[ques_id]=final_score
+	print final_score
+	# with open("nlp_score_list.dump","wb") as f3:
+	# 	pickle.dump(nlp_score_list,f3)
+	f.write(ques_id+" "+str(final_score)+"\n")
+
 
 f.close()
